99001948746559fa4c84031705a5959957ff399f
thongtq69 <thongtqse173366@fpt.edu.vn>
2026-02-15 08:45:37 +0700

    optimize favicon for google search results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,12 +72,20 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.ico', sizes: '32x32' },
       { url: '/favicon.png', type: 'image/png', sizes: '32x32' },
       { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
     ],
+    shortcut: '/favicon.ico',
     apple: [
       { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/apple-icon.png',
+      },
     ],
   },
   manifest: '/site.webmanifest',
